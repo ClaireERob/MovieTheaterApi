18292ffb4c82940ce4bd932ecfd780010004238f
Claire Robson <claire.robson1@barclays.com>
2024-10-18 09:11:53 +0100

    Set up route handlers

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
+
+
+
+// GET all users
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
+
+//GET one user by ID
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
